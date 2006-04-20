ce30a44a5ca5e99a5141b3acac01277e68eaaa8c
Greg Beaver <cellog@php.net>
2006-04-20 03:10:59 +0000

    opendir could match files, so the file and search path must be identical to match a directory
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_Archive/trunk@211745 c90b9560-bf6c-de11-be94-00142212c4b1

--- Archive.php
+++ Archive.php
@@ -581,7 +581,8 @@ class PHP_Archive
                 if (strpos($fname, '/')) {
                     $a = explode('/', $fname);
                     $this->_dirFiles[array_shift($a)] = true;
-                } else {
+                } elseif (strlen($file) == strlen($path)) {
+                    // must be exact match, otherwise it's a file
                     $this->_dirFiles[$fname] = true;
                 }
             }
