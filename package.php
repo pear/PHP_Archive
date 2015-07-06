6d4fdee4525b5986d2f16abf900f39dbd34c95af
Christian Weiske <cweiske@cweiske.de>
2015-07-06 07:47:51 +0200

    Prepare release of 0.12.0

--- package.php
+++ package.php
@@ -6,7 +6,12 @@ $version = '0.12.0';
 $apiversion = '1.1.0';
 $notes = '
 sync with ext/phar
- * add empty directory support';
+* add empty directory support
+
+Fix bugs
+* Get rid of ereg_replace
+* Fix phar compression detection
+* Make tests run on PHP 5.2 - 7';
 
 
 $package = PEAR_PackageFileManager2::importOptions(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'package.xml',
@@ -14,7 +19,7 @@ $package = PEAR_PackageFileManager2::importOptions(dirname(__FILE__) . DIRECTORY
     'ignore'            => array('package.php', 'package.xml', 'package2.xml', '*.bak', '*src*',
         '*.tgz', '*pear_media*', 'index.htm', 'PEAR.phar', 'docs/', 'phar_unpack.php', 'info.phar',
         '*CVS*'),
-	'filelistgenerator' => 'cvs', // other option is 'file'
+	'filelistgenerator' => 'file', // other option is 'file'
     'changelogoldtonew' => false,
     'baseinstalldir'    => 'PHP',
     'packagedirectory'  => dirname(__FILE__),
@@ -27,9 +32,9 @@ $package->setReleaseStability('alpha');
 $package->setAPIStability('stable');
 $package->setNotes($notes);
 $package->clearDeps();
-$package->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.4.3');
-$package->setPhpDep('5.1.0');
-$package->setPearinstallerDep('1.4.3');
+$package->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.9.5');
+$package->setPhpDep('5.2.0');
+$package->setPearinstallerDep('1.9.0');
 $package->addReplacement('Archive.php', 'package-info', '@API-VER@', 'api-version');
 $package->addReplacement('Archive/Creator.php', 'package-info', '@API-VER@', 'api-version');
 $package->addReplacement('Archive/Creator.php', 'pear-config', '@data_dir@', 'data_dir');
